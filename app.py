c674c635222719fa9b0013b7a74dfd8adc3e0af2
RANJENI C <cranjeni1@gmail.com>
2025-06-21 23:13:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-##from sentence_transformers import SentenceTransformer
-#import faiss
+from sentence_transformers import SentenceTransformer
+import faiss
 import requests
 from typing import List, Tuple
 from io import StringIO
